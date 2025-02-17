c6eafda06dbfe86451a3e5ddf4ace245f4330be2
Naisheel Shah <67424908+nshah1503@users.noreply.github.com>
2025-02-17 15:09:08 -0800

    added timer class

---
+++ MultithreadingConcepts/bits/Timer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Naisheel on 2/17/25.
+//
+
+#include "Timer.h"
---
+++ MultithreadingConcepts/bits/Timer.h
@@ -0,0 +1,16 @@
+//
+// Created by Naisheel on 2/17/25.
+//
+
+#ifndef TIMER_H
+#define TIMER_H
+
+
+
+class Timer {
+
+};
+
+
+
+#endif //TIMER_H
