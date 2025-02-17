ecfe4cf8f931d7d3c4d033f1333c14aad4c69702
Naisheel Shah <67424908+nshah1503@users.noreply.github.com>
2025-02-17 15:19:38 -0800

    header definitions

--- MultithreadingConcepts/bits/Timer.h
+++ MultithreadingConcepts/bits/Timer.h
@@ -4,11 +4,24 @@
 
 #ifndef TIMER_H
 #define TIMER_H
+#include "stdc++.h"
 
 
+struct Timer {
+  std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+  std::chrono::duration<float> duration;
 
-class Timer {
-
+  Timer()
+  {
+    start = std::chrono::high_resolution_clock::now();
+  }
+  ~Timer()
+  {
+    end = std::chrono::high_resolution_clock::now();
+    duration = end-start;
+    const float ms = duration.count()*100;
+    std::cout << "The process took: " << ms << std::endl;
+  }
 };
 
 
